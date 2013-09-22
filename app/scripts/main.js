ef97ca83190141cd24f308a1ab2948ee3c086278
Thomas I. Sample <thomas@limitlessimaginationstudios>
2013-09-21 21:49:54 -0400

    photograph css added to Wedding, removed NinaWray Logo, New Homepage with Carousel

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('\'Allo \'Allo!');
+!function ($) {
+    // Handler for .ready() called
+    $(function(){
+        // carousel demo
+        $('#myCarousel').carousel({
+          interval: 10000
+        });
+        console.log('Initialized Carousel with 2 second interval:');
+        console.log('[  $(\'#myCarousel\').carousel({ interval: 2000 });  ]');
+    });
+}(window.jQuery);
